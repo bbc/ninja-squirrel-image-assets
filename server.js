ffc773fee8fa19ee6317eabdf8e6e4456320ea8e
John Beech <johnbeech.github@mkv25.net>
2019-08-25 07:34:23 +0100

    Add package.json and open command

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const path = require('path')
+const open = require('open')
 const app = express()
 
 app.use('/redesign', express.static(path.join(__dirname, 'redesign')))
 
 const port = 8900
-app.listen(port, () => {
-  console.log('Preview server on:', `http://localhost:${port}/redesign/`)
+app.listen(port, async () => {
+  const localUri = `http://localhost:${port}/redesign/`
+  console.log('Preview server on:', localUri)
+  await open(localUri)
 })
