6fc1c242489b5a2f4dce0b727738bdf18eec4659
John Beech <johnbeech.github@mkv25.net>
2019-08-25 22:54:17 +0100

    Add buttons based on layer names to toggle layers

--- redesign/scripts/navigate-svg.js
+++ redesign/scripts/navigate-svg.js
@@ -1,11 +1,29 @@
 document.addEventListener('imgReplacedWithSvg', analyzeSVG)
 
+function insertAfter(newNode, referenceNode) {
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
 function analyzeSVG(ev) {
   const svg = ev.detail.svg
   console.log('Found SVG', svg, 'to analyze')
 
   const layers = Array.from(svg.querySelectorAll('g')).filter(g => g.getAttribute('inkscape:groupmode') === 'layer')
-  const layerNames = layers.map((layer) => layer.getAttribute('inkscape:label'))
+  const buttonContainer = document.createElement('div')
 
-  console.log('Found layers names', layerNames)
+  const buttonTitle = document.createElement('h3')
+  buttonTitle.innerText = 'Toggle Layer Visibility'
+  buttonContainer.appendChild(buttonTitle)
+
+  layers.forEach(layer => {
+    const button = document.createElement('button')
+    const layerName = layer.getAttribute('inkscape:label')
+    button.innerText = layerName
+    buttonContainer.appendChild(button)
+    button.addEventListener('click', (ev) => {
+      layer.style.display = layer.style.display === 'none' ?  'inline': 'none';
+    })
+  })
+
+  insertAfter(buttonContainer, svg)
 }
