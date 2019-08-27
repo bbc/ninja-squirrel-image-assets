b94bd48b82c3371e29f2008ceb852cfcc601e1b2
John Beech <john.beech@bbc.co.uk>
2019-08-27 15:10:00 +0100

    Restructure files into named folders

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const path = require('path')
 const open = require('open')
 const app = express()
 
-app.use('/redesign', express.static(path.join(__dirname, 'redesign')))
+app.use('/designer', express.static(path.join(__dirname, 'designer')))
+app.use('/svg', express.static(path.join(__dirname, 'svg')))
 
 const port = 8900
 app.listen(port, async () => {
-  const localUri = `http://localhost:${port}/redesign/`
+  const localUri = `http://localhost:${port}/designer/`
   console.log('Preview server on:', localUri)
   await open(localUri)
 })
